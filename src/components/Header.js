859727ea6d609944279d102b4acf9999acdaf86d
MonteyMontey <32668472+MonteyMontey@users.noreply.github.com>
2019-10-02 16:40:46 +0200

    add header & remove css reset

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Navbar, Container, Form, FormControl, Button} from 'react-bootstrap';
+
+export default function Header() {
+  return (
+    <Navbar style={{padding: '10px'}} bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand style={{fontSize: '25px'}} href="#home">Social-Network</Navbar.Brand>
+        <Form inline>
+          <FormControl type="text" placeholder="Username" className=" mr-sm-2" />
+          <FormControl type="password" placeholder="Password" className=" mr-sm-2" />
+          <Button type="submit">Log In</Button>
+        </Form>
+      </Container>
+    </Navbar>
+  )
+}
