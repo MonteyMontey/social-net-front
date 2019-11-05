70c087474762a231488a170ae7564dfea6d9bcd7
monteymontey <dominik.klein@code.berlin>
2019-11-05 17:09:21 +0100

    add front end validation

--- src/components/landing-page/Registration.js
+++ src/components/landing-page/Registration.js
@@ -1,41 +1,55 @@
 import React from "react";
-import { Container, Form, Button, Col } from "react-bootstrap";
+import { Container, Form, Button, Col, Alert } from "react-bootstrap";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import Axios from 'axios';
 
 class Registration extends React.Component {
   state = {
-    date: new Date(),
-    gender: ""
+    date: new Date("August 3, 1997"),
+    gender: "Male",
+    showPasswordsDontMatchWarning: false
   };
 
+  PasswordsDontMatchWarning = () => {
+    return (
+      <Alert dismissible
+        onClose={() => this.setState({ showPasswordsDontMatchWarning: false })}
+        variant="danger">
+        Your passwords don't match!
+      </Alert>
+    )
+  }
+
   submitRegistration = e => {
-    //TODO: validate form (check if empty)
+    if (e.target.formGridPassword.value !== e.target.formGridRepeatPassword.value) {
+      this.setState({ showPasswordsDontMatchWarning: true });
+    } else {
 
-    let registration = {}
-    registration.firstName = e.target.formGridFirstName.value
-    registration.lastName = e.target.formGridLastName.value
-    registration.email = e.target.formGridEmail.value
-    registration.password = e.target.formGridPassword.value
-    registration.birthday = e.target.datePicker.value
-    registration.gender = this.state.gender
+      let registration = {}
+      registration.firstName = e.target.formGridFirstName.value
+      registration.lastName = e.target.formGridLastName.value
+      registration.email = e.target.formGridEmail.value
+      registration.password = e.target.formGridPassword.value
+      registration.birthday = e.target.datePicker.value
+      registration.gender = this.state.gender
 
-    Axios.post('/registration', registration)
-    .then(res => {
-      console.log("Successfully sent registration data to backend", registration)
-      console.log(res)
-    })
-    .catch(err => {
-      console.error("Failed to send registration data to backend", err)
-    })
-
+      Axios.post('/registration', registration)
+        .then(res => {
+          console.log("Successfully sent registration data to backend", registration)
+          console.log(res)
+        })
+        .catch(err => {
+          console.error("Failed to send registration data to backend", err)
+        })
+    }
     e.preventDefault()
   }
 
   render() {
     return (
       <div style={{ backgroundColor: "#e9ebee", height: "100%", width: "100%" }}>
+        {this.state.showPasswordsDontMatchWarning ? <this.PasswordsDontMatchWarning /> : null}
         <Container>
           <div style={styles.registration}>
             <h3 style={{ color: "#343a40" }}>Create an account</h3>
@@ -44,41 +58,71 @@ class Registration extends React.Component {
 
               <Form.Row>
                 <Form.Group as={Col} controlId="formGridFirstName">
-                  <Form.Control required type="text" placeholder="First Name" />
+                  <Form.Control required
+                    type="text"
+                    placeholder="First Name"
+                    pattern="[a-zA-Z_0-9]*"
+                    minlength="3"
+                    maxlength="20"
+                    title="[a-zA-Z_0-9]*" />
                 </Form.Group>
 
                 <Form.Group as={Col} controlId="formGridLastName">
-                  <Form.Control required type="text" placeholder="Last Name" />
+                  <Form.Control required
+                    type="text"
+                    placeholder="Last Name"
+                    pattern="[a-zA-Z_0-9]*"
+                    minlength="3"
+                    maxlength="20"
+                    title="[a-zA-Z_0-9]*" />
                 </Form.Group>
               </Form.Row>
 
               <Form.Row>
                 <Form.Group as={Col} controlId="formGridEmail">
-                  <Form.Control required style={{ width: "50%" }} type="email" placeholder="Email" />
+                  <Form.Control required
+                    style={{ width: "50%" }}
+                    type="email"
+                    placeholder="Email"
+                    maxlength="254" />
                 </Form.Group>
               </Form.Row>
 
               <Form.Row>
                 <Form.Group as={Col} controlId="formGridPassword">
-                  <Form.Control required type="password" placeholder="Password" />
+                  <Form.Control required
+                    type="password"
+                    placeholder="Password"
+                    minlength="8"
+                    maxlength="25"
+                    pattern="[a-zA-Z0-9_$!%^*#/\()?]*"
+                    title="[a-zA-Z0-9_$!%^*#/\()?]*" />
                 </Form.Group>
 
                 <Form.Group as={Col} controlId="formGridRepeatPassword">
-                  <Form.Control required type="password" placeholder="Repeat password" />
+                  <Form.Control required
+                    type="password"
+                    placeholder="Repeat password"
+                    minlength="8"
+                    maxlength="25"
+                    pattern="[a-zA-Z0-9_$!%^*#/\()?]*"
+                    title="[a-zA-Z0-9_$!%^*#/\()?]*" />
                 </Form.Group>
               </Form.Row>
 
               <h5 style={{ marginBottom: '20px', marginTop: '10px', color: "#343a40" }}>Birthday</h5>
 
               <Form.Row>
-                <DatePicker id="datePicker" selected={this.state.date} onChange={(newDate) => this.setState({date: newDate})} />
+                <DatePicker id="datePicker"
+                  selected={this.state.date}
+                  onChange={(newDate) => this.setState({ date: newDate })} />
               </Form.Row>
 
               <Form.Row>
                 <div style={{ margin: "30px 0 0 0" }} key={'inline-radio'} className="mb-3">
                   <Form.Check
                     checked={this.state.gender === "Male"}
-                    onChange={() => this.setState({gender: "Male"})}
+                    onChange={() => this.setState({ gender: "Male" })}
                     inline
                     label="Male"
                     type='radio'
@@ -86,7 +130,7 @@ class Registration extends React.Component {
                   />
                   <Form.Check
                     checked={this.state.gender === "Female"}
-                    onChange={() => this.setState({gender: "Female"})}
+                    onChange={() => this.setState({ gender: "Female" })}
                     inline
                     label="Female"
                     type='radio'
@@ -94,7 +138,7 @@ class Registration extends React.Component {
                   />
                   <Form.Check
                     checked={this.state.gender === "Other"}
-                    onChange={() => this.setState({gender: "Other"})}
+                    onChange={() => this.setState({ gender: "Other" })}
                     inline
                     label="Other"
                     type='radio'
@@ -120,7 +164,7 @@ const styles = {
   },
   registration: {
     paddingTop: "50px",
-    paddingBottom: "200px"
+    paddingBottom: "130px"
   }
 };
 
