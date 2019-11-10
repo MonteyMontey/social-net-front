763271e5e0e7a0f5314a6f42351044f1e919ff77
monteymontey <dominik.klein@code.berlin>
2019-11-10 15:24:08 +0100

    send post to backend

--- src/components/front-page/CreatePostPrompt.js
+++ src/components/front-page/CreatePostPrompt.js
@@ -1,16 +1,37 @@
 import React from 'react'
-import {Form, Button } from "react-bootstrap";
+import { Form, Button } from "react-bootstrap";
 import InfiniteScroll from 'react-infinite-scroll-component';
+import Axios from 'axios';
 
 class CreatePostPrompt extends React.Component {
 
+  submitPost = e => {
+    let post = {};
+    post.user = "PLACEHOLDER_USER_ID";
+    post.body = e.target.post.value;
+
+    Axios.post('/post', post)
+      .then(res => {
+        console.log("Successfully sent post data to backend", res);
+        // go on ...
+      })
+      .catch((error) => {
+        console.log("Something went wrong", error.response);
+      });
+
+    e.preventDefault()
+  }
+
   render() {
     return (
-      <Form style={{borderStyle: "solid", borderWidth: "1px", borderColor: "LightGray", borderRadius: "5px",
-       marginTop: "20px", marginBottom: "50px", padding: "15px", backgroundColor: "white"}}>
+      <Form onSubmit={this.submitPost}
+        style={{
+          borderStyle: "solid", borderWidth: "1px", borderColor: "LightGray", borderRadius: "5px",
+          marginTop: "20px", marginBottom: "50px", padding: "15px", backgroundColor: "white"
+        }}>
         <Form.Group>
-          <Form.Control as="textarea" rows="3" maxLength={4}
-            placeholder="What did you do today?" />
+          <Form.Control id="post" as="textarea" rows="3" maxLength={280}
+            placeholder="What's on your mind?" />
         </Form.Group>
         <Button variant="primary" type="submit">Post</Button>
       </Form>
