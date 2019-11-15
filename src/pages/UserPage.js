6a730b6c12e85349c748a542137a82498393404b
monteymontey <dominik.klein@code.berlin>
2019-11-15 19:42:54 +0100

    fix

--- src/pages/UserPage.js
+++ src/pages/UserPage.js
@@ -3,8 +3,34 @@ import userProfile from '../images/user_profile.png';
 import Header from '../components/front-page/Header';
 import ScrollFeed from '../components/front-page/ScrollFeed';
 import { Container, Row, Col, Button } from "react-bootstrap";
+import Axios from 'axios';
 
 class Home extends React.Component {
+
+  state = {
+
+  }
+
+  componentDidMount() {
+    this.setState({
+      userID: this.props.match.params.userId
+    });
+
+    Axios.get('/users', {
+      params: {
+        id: this.props.match.params.userId
+      }
+    })
+      .then(res => {
+        console.log("res", res)
+        this.setState({
+          name: res.data.firstName + " " + res.data.lastName
+        });
+      })
+      .catch((error) => {
+        console.error(error.response);
+      });
+  }
 
   render() {
     return (
@@ -16,10 +42,10 @@ class Home extends React.Component {
               <div>
                 <img style={{ marginTop: "50px", display: "block", marginRight: "auto", marginLeft: "auto", height: "400px", width: "400px" }}
                   alt="profile" src={userProfile} />
-                  <div style={{marginTop: "25px", textAlign: "center"}}>
-                    <h1>Dominik Klein</h1>
-                    <Button variant="outline-primary">Add <i style={{marginLeft: "5px"}} className="ml-sm-1 fas fa-user-plus"></i></Button>
-                  </div>
+                <div style={{ marginTop: "25px", textAlign: "center" }}>
+                  <h1>{this.state.name}</h1>
+                  <Button  style={{ marginTop: "10px" }} variant="primary">Add <i className="ml-sm-2 fas fa-user-plus"></i></Button>
+                </div>
               </div>
             </Col>
             <Col xs={5}>
