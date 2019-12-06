f67e827ccb9499b7ebddc0220ffd777a3c6b4984
monteymontey <dominik.klein@code.berlin>
2019-12-06 16:09:40 +0100

    redirect from login to frontpage if access_token valid

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import Landing from './components/pages/LandingPage/LandingPage';
 import Home from './components/pages/FrontPage/FrontPage';
 import User from './components/pages/UserPage/UserPage';
 import withAuth from './components/auth/withAuth';
+import withoutAuth from './components/auth/withoutAuth';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <Router forceRefresh={true}>
       <Switch>
-        <Route path="/" component={Landing} exact />
+        <Route path="/" component={withoutAuth(Landing)} exact />
         <Route path="/frontpage" component={withAuth(Home)} exact />
         <Route path="/user/:userId" component={withAuth(User)} exact />
       </Switch>
