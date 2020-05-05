34e0578563da77fafb64b2797805937ee5518501
monteymontey <dominik.klein@code.berlin>
2020-05-05 19:54:33 +0200

    remove error throw

--- src/components/auth/withAuth.jsx
+++ src/components/auth/withAuth.jsx
@@ -18,8 +18,8 @@ export default function withAuth(ComponentToProtect) {
           if (res.status === 200) {
             this.setState({ loading: false });
           } else {
-            const error = new Error(res.error);
-            throw error;
+            //const error = new Error(res.error);
+            //throw error;
           }
         })
         .catch(err => {
